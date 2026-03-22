d3fdcd06d4b080f1555df4450e78fc9bf84e2be5
Razvan Drutu <drutu.razvan2004@gmail.com>
2026-03-23 01:25:17 +0200

    fix: correct leaflet module augmentation to avoid shadowing core types

--- src/types/leaflet.vectorgrid.d.ts
+++ src/types/leaflet.vectorgrid.d.ts
@@ -1,25 +1,9 @@
-declare module 'leaflet.vectorgrid' {
-  import * as L from 'leaflet';
+import * as L from 'leaflet';
 
-  namespace VectorGrid {
-    interface ProtobufOptions extends L.LayerOptions {
-      vectorTileLayerStyles?: {
-        [key: string]: any;
-      };
-      minZoom?: number;
-      maxZoom?: number;
-      attribution?: string;
-    }
-
-    function protobuf(url: string, options?: ProtobufOptions): L.Layer;
-  }
-
-  export default VectorGrid;
-}
-
-// Extindem tipul Leaflet global L pentru a include vectorGrid
 declare module 'leaflet' {
-  export const vectorGrid: {
-    protobuf: (url: string, options?: any) => any;
-  };
+  namespace vectorGrid {
+    function protobuf(url: string, options?: any): L.Layer;
+  }
 }
+
+declare module 'leaflet.vectorgrid';
